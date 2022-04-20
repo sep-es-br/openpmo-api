36dcf307a4ff1cfaf847c934a4acab9366d4cbab
icaromartinss <icaromartinss@hotmail.com>
2022-04-20 12:22:04 -0400

    Adjust Homologação

--- src/main/java/br/gov/es/openpmo/repository/IsEvaluatedByRepository.java
+++ src/main/java/br/gov/es/openpmo/repository/IsEvaluatedByRepository.java
@@ -1,25 +1,33 @@
 package br.gov.es.openpmo.repository;
 
+import br.gov.es.openpmo.model.actors.Person;
 import br.gov.es.openpmo.model.relations.IsEvaluatedBy;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IsEvaluatedByRepository extends Neo4jRepository<IsEvaluatedBy, Long> {
 
-  @Query("MATCH (person:Person)<-[isEvaluatedBy:IS_EVALUATED_BY]-(baseline:Baseline) " +
-         "WHERE id(baseline)=$idBaseline AND id(person)=$idPerson " +
-         "RETURN person, isEvaluatedBy, baseline")
-  Optional<IsEvaluatedBy> findEvaluation(Long idBaseline, Long idPerson);
+    @Query("MATCH (person:Person)<-[isEvaluatedBy:IS_EVALUATED_BY]-(baseline:Baseline) " +
+            "WHERE id(baseline)=$idBaseline AND id(person)=$idPerson " +
+            "RETURN person, isEvaluatedBy, baseline")
+    Optional<IsEvaluatedBy> findEvaluation(Long idBaseline, Long idPerson);
 
-  @Query("MATCH (workpack:Workpack)-[:IS_BASELINED_BY]->(baseline:Baseline) " +
-         "WHERE id(baseline)=$idBaseline " +
-         "OPTIONAL MATCH (member:Person)-[isCCBMemberFor:IS_CCB_MEMBER_FOR{active:true}]->(workpack) " +
-         "OPTIONAL MATCH (evaluator:Person)<-[:IS_EVALUATED_BY]-(baseline) " +
-         "WITH count(DISTINCT evaluator) AS evaluators, count(DISTINCT member) AS members " +
-         "RETURN evaluators = members")
-  boolean wasEvaluatedByAllMembers(Long idBaseline);
+    @Query("MATCH (person:Person)<-[isEvaluatedBy:IS_EVALUATED_BY]-(baseline:Baseline)" +
+            "<-[isBaselinedBy:IS_BASELINED_BY]-(workpack:Workpack) " +
+            "WHERE id(baseline)=$idBaseline " +
+            "RETURN person, isEvaluatedBy, baseline, isBaselinedBy, workpack")
+    List<Person> findEvaluators(Long idBaseline);
+
+    @Query("MATCH (workpack:Workpack)-[:IS_BASELINED_BY]->(baseline:Baseline) " +
+            "WHERE id(baseline)=$idBaseline " +
+            "OPTIONAL MATCH (member:Person)-[isCCBMemberFor:IS_CCB_MEMBER_FOR{active:true}]->(workpack) " +
+            "OPTIONAL MATCH (evaluator:Person)<-[:IS_EVALUATED_BY]-(baseline) " +
+            "WITH count(DISTINCT evaluator) AS evaluators, count(DISTINCT member) AS members " +
+            "RETURN evaluators = members")
+    boolean wasEvaluatedByAllMembers(Long idBaseline);
 }
--- src/main/java/br/gov/es/openpmo/service/baselines/GetAllBaselineEvaluations.java
+++ src/main/java/br/gov/es/openpmo/service/baselines/GetAllBaselineEvaluations.java
@@ -3,7 +3,6 @@ package br.gov.es.openpmo.service.baselines;
 import br.gov.es.openpmo.dto.baselines.EvaluationItem;
 import br.gov.es.openpmo.model.actors.Person;
 import br.gov.es.openpmo.model.relations.IsEvaluatedBy;
-import br.gov.es.openpmo.repository.IsCCBMemberRepository;
 import br.gov.es.openpmo.repository.IsEvaluatedByRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,37 +14,33 @@ import java.util.stream.Collectors;
 @Component
 public class GetAllBaselineEvaluations implements IGetAllBaselineEvaluations {
 
-  private final IsEvaluatedByRepository evaluatedByRepository;
-  private final IsCCBMemberRepository ccbMemberRepository;
+    private final IsEvaluatedByRepository evaluatedByRepository;
 
-  @Autowired
-  public GetAllBaselineEvaluations(
-    final IsEvaluatedByRepository evaluatedByRepository,
-    final IsCCBMemberRepository ccbMemberRepository
-  ) {
-    this.evaluatedByRepository = evaluatedByRepository;
-    this.ccbMemberRepository = ccbMemberRepository;
-  }
+    @Autowired
+    public GetAllBaselineEvaluations(final IsEvaluatedByRepository evaluatedByRepository) {
+        this.evaluatedByRepository = evaluatedByRepository;
+    }
 
-  @Override public List<EvaluationItem> getEvaluations(final Long idBaseline) {
-    final List<Person> members = this.getMembers(idBaseline);
-    return members.stream()
-      .map(member -> this.getEvaluationItem(member, idBaseline))
-      .collect(Collectors.toList());
-  }
+    @Override
+    public List<EvaluationItem> getEvaluations(final Long idBaseline) {
+        final List<Person> members = this.getMembers(idBaseline);
+        return members.stream()
+                .map(member -> this.getEvaluationItem(member, idBaseline))
+                .collect(Collectors.toList());
+    }
 
-  private EvaluationItem getEvaluationItem(final Person member, final Long idBaseline) {
-    final Optional<IsEvaluatedBy> maybeEvaluation = this.getEvaluation(member, idBaseline);
-    return maybeEvaluation.map(EvaluationItem::fromBaselineEvaluated)
-      .orElseGet(() -> EvaluationItem.fromBaselineNotEvaluated(member));
-  }
+    private EvaluationItem getEvaluationItem(final Person member, final Long idBaseline) {
+        final Optional<IsEvaluatedBy> maybeEvaluation = this.getEvaluation(member, idBaseline);
+        return maybeEvaluation.map(EvaluationItem::fromBaselineEvaluated)
+                .orElseGet(() -> EvaluationItem.fromBaselineNotEvaluated(member));
+    }
 
-  private Optional<IsEvaluatedBy> getEvaluation(final Person member, final Long idBaseline) {
-    return this.evaluatedByRepository.findEvaluation(idBaseline, member.getId());
-  }
+    private Optional<IsEvaluatedBy> getEvaluation(final Person member, final Long idBaseline) {
+        return this.evaluatedByRepository.findEvaluation(idBaseline, member.getId());
+    }
 
-  private List<Person> getMembers(final Long idBaseline) {
-    return this.ccbMemberRepository.findAllActiveMembersOfBaseline(idBaseline);
-  }
+    private List<Person> getMembers(final Long idBaseline) {
+        return this.evaluatedByRepository.findEvaluators(idBaseline);
+    }
 
 }
