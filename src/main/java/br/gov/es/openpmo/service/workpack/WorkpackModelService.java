1cebb97b56a9dcfe4dcd40bcf940e84d3419a37e
icaromartinss <icaromartinss@hotmail.com>
2022-06-06 09:26:25 -0400

    Ajustes homologação

--- src/main/java/br/gov/es/openpmo/service/workpack/WorkpackModelService.java
+++ src/main/java/br/gov/es/openpmo/service/workpack/WorkpackModelService.java
@@ -1,30 +1,13 @@
 package br.gov.es.openpmo.service.workpack;
 
-import br.gov.es.openpmo.dto.workpackmodel.DeliverableModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.MilestoneModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.OrganizerModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.PortfolioModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.ProgramModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.ProjectModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.WorkpackModelDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.DeliverableModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.MilestoneModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.OrganizerModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.PortfolioModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.ProgramModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.ProjectModelDetailDto;
-import br.gov.es.openpmo.dto.workpackmodel.details.WorkpackModelDetailDto;
+import br.gov.es.openpmo.dto.workpackmodel.*;
+import br.gov.es.openpmo.dto.workpackmodel.details.*;
 import br.gov.es.openpmo.dto.workpackmodel.params.WorkpackModelParamDto;
 import br.gov.es.openpmo.dto.workpackmodel.params.properties.*;
+import br.gov.es.openpmo.enumerator.Session;
 import br.gov.es.openpmo.exception.NegocioException;
 import br.gov.es.openpmo.model.properties.models.*;
-import br.gov.es.openpmo.model.workpacks.models.DeliverableModel;
-import br.gov.es.openpmo.model.workpacks.models.MilestoneModel;
-import br.gov.es.openpmo.model.workpacks.models.OrganizerModel;
-import br.gov.es.openpmo.model.workpacks.models.PortfolioModel;
-import br.gov.es.openpmo.model.workpacks.models.ProgramModel;
-import br.gov.es.openpmo.model.workpacks.models.ProjectModel;
-import br.gov.es.openpmo.model.workpacks.models.WorkpackModel;
+import br.gov.es.openpmo.model.workpacks.models.*;
 import br.gov.es.openpmo.repository.WorkpackModelRepository;
 import br.gov.es.openpmo.service.actors.OrganizationService;
 import br.gov.es.openpmo.service.office.DomainService;
@@ -39,21 +22,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import static br.gov.es.openpmo.utils.ApplicationMessage.PROPERTY_MODEL_DELETE_RELATIONSHIP_ERROR;
-import static br.gov.es.openpmo.utils.ApplicationMessage.PROPERTY_UPDATE_TYPE_ERROR;
-import static br.gov.es.openpmo.utils.ApplicationMessage.WORKPACKMODEL_NOT_FOUND;
-import static br.gov.es.openpmo.utils.ApplicationMessage.WORKPACK_MODEL_MILESTONE_DELIVERABLE_PROGRAM_ERROR;
+import static br.gov.es.openpmo.utils.ApplicationMessage.*;
 
 @Service
 public class WorkpackModelService implements BreadcrumbWorkpackModelHelper {
@@ -410,9 +383,9 @@ public class WorkpackModelService implements BreadcrumbWorkpackModelHelper {
       if(workpackModelParamDto.getSortBy() != null && !CollectionUtils.isEmpty(workpackModel.getProperties())) {
         workpackModel.setSortBy(
           workpackModel.getProperties().stream()
-            .filter(p ->
-                      p.getLabel() != null && p.getLabel().equals(workpackModelParamDto.getSortBy())
-            ).findFirst().orElse(null)
+            .filter(p -> p.getSession() == Session.PROPERTIES)
+            .filter(p -> p.getName() != null && p.getName().equals(workpackModelParamDto.getSortBy()))
+            .findFirst().orElse(null)
         );
       }
     }
