c08526deca3d3fefddb89aba17a98302eab40009
da0hn <gabrielhondacba@gmail.com>
2022-08-18 09:15:17 -0400

    Adjust Homologação

--- src/main/java/br/gov/es/openpmo/dto/baselines/ccbmemberview/UnitCostCalculator.java
+++ src/main/java/br/gov/es/openpmo/dto/baselines/ccbmemberview/UnitCostCalculator.java
@@ -5,45 +5,52 @@ import java.math.RoundingMode;
 
 public final class UnitCostCalculator {
 
-    private final BigDecimal foreseenCost;
-    private final BigDecimal foreseenWork;
-    private final BigDecimal plannedCost;
-    private final BigDecimal plannedWork;
+  private final BigDecimal foreseenCost;
+  private final BigDecimal foreseenWork;
+  private final BigDecimal plannedCost;
+  private final BigDecimal plannedWork;
 
-    public UnitCostCalculator(
-            final BigDecimal foreseenCost,
-            final BigDecimal foreseenWork,
-            final BigDecimal plannedCost,
-            final BigDecimal plannedWork
-    ) {
-        this.foreseenCost = foreseenCost;
-        this.foreseenWork = foreseenWork;
-        this.plannedCost = plannedCost;
-        this.plannedWork = plannedWork;
-    }
+  public UnitCostCalculator(
+    final BigDecimal foreseenCost,
+    final BigDecimal foreseenWork,
+    final BigDecimal plannedCost,
+    final BigDecimal plannedWork
+  ) {
+    this.foreseenCost = foreseenCost;
+    this.foreseenWork = foreseenWork;
+    this.plannedCost = plannedCost;
+    this.plannedWork = plannedWork;
+  }
 
-    private static boolean HasNoCurrentNorProposedValueToCalculateUnitCost(
-            final Comparable<? super BigDecimal> forseenCost,
-            final Comparable<? super BigDecimal> plannedCost
-    ) {
-        return hasValue(forseenCost) && hasValue(plannedCost);
-    }
+  private static boolean HasNoCurrentNorProposedValueToCalculateUnitCost(
+    final Comparable<? super BigDecimal> forseenCost,
+    final Comparable<? super BigDecimal> plannedCost
+  ) {
+    return hasValue(forseenCost) && hasValue(plannedCost);
+  }
 
-    private static boolean hasValue(final Comparable<? super BigDecimal> value) {
-        return value != null && value.compareTo(BigDecimal.ZERO) == 0;
-    }
+  private static boolean hasValue(final Comparable<? super BigDecimal> value) {
+    return value != null && value.compareTo(BigDecimal.ZERO) == 0;
+  }
 
-    public BigDecimal calculate() {
-        if (HasNoCurrentNorProposedValueToCalculateUnitCost(this.foreseenCost, this.plannedCost)) {
-            return BigDecimal.ONE;
-        }
-        if (this.plannedCost != null) {
-            return this.plannedCost.divide(this.plannedWork, 6, RoundingMode.HALF_UP);
-        }
-        if (this.foreseenCost != null) {
-            return this.foreseenCost.divide(this.foreseenWork, 6, RoundingMode.HALF_UP);
-        }
-        return BigDecimal.ONE;
+  private static BigDecimal getOneIfValueZero(final BigDecimal value) {
+    if(BigDecimal.ZERO.compareTo(value) == 0) {
+      return BigDecimal.ONE;
     }
+    return value;
+  }
+
+  public BigDecimal calculate() {
+    if(HasNoCurrentNorProposedValueToCalculateUnitCost(this.foreseenCost, this.plannedCost)) {
+      return BigDecimal.ONE;
+    }
+    if(this.plannedCost != null) {
+      return this.plannedCost.divide(getOneIfValueZero(this.plannedWork), 6, RoundingMode.HALF_UP);
+    }
+    if(this.foreseenCost != null) {
+      return this.foreseenCost.divide(getOneIfValueZero(this.foreseenWork), 6, RoundingMode.HALF_UP);
+    }
+    return BigDecimal.ONE;
+  }
 
 }
