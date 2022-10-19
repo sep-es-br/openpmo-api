8ae47c13d3d1980a339561747292b72cff82e379
Vagner Dargan Cordeiro <vagner.cordeiro@sep.local>
2022-10-19 14:24:03 -0300

    Initial upgrades

--- src/main/java/br/gov/es/openpmo/repository/AuthServiceRepository.java
+++ src/main/java/br/gov/es/openpmo/repository/AuthServiceRepository.java
@@ -2,11 +2,12 @@ package br.gov.es.openpmo.repository;
 
 import br.gov.es.openpmo.model.actors.AuthService;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AuthServiceRepository extends Neo4jRepository<AuthService, Long> {
 
-  Optional<AuthService> findAuthServiceByServer(String server);
+  Optional<AuthService> findAuthServiceByServer(@Param("server") String server);
 
 }
