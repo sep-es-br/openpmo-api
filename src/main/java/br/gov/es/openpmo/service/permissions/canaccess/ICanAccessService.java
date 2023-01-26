a1810d4e34a88279db13b180759951fa21ea651b
Marcos Antonio dos Santos <marcos.santos@sep.local>
2023-01-26 16:53:09 -0300

    correção canaccess

--- src/main/java/br/gov/es/openpmo/service/permissions/canaccess/ICanAccessService.java
+++ src/main/java/br/gov/es/openpmo/service/permissions/canaccess/ICanAccessService.java
@@ -4,37 +4,43 @@ import java.util.List;
 
 public interface ICanAccessService {
 
-    void ensureCanReadResource(
-            Long id,
-            String authorization);
+  void ensureCanReadResource(
+    Long id,
+    String authorization
+  );
 
-    void ensureCanEditResource(
-            Long id,
-            String authorization);
+  void ensureCanEditResource(
+    Long id,
+    String authorization
+  );
 
-    void ensureIsAdministrator(String authorization);
+  void ensureIsAdministrator(String authorization);
 
-    void ensureCanAccessSelfResource(
-            Long idPerson,
-            String authorization);
+  void ensureCanAccessSelfResource(
+    Long idPerson,
+    String authorization
+  );
 
-    void ensureCanAccessManagementResource(
-            Long id,
-            String authorization);
+  void ensureCanAccessManagementResource(
+    Long id,
+    String authorization
+  );
 
-    void ensureCanAccessManagementOrSelfResource(
-            List<Long> ids,
-            String authorization);
 
-    void ensureCanReadManagementResource(
-            Long idOffice,
-            String key,
-            String authorization);
+  void ensureCanAccessManagementOrSelfResource(
+    List<Long> ids,
+    String authorization
+  );
 
-    void ensureCanEditResource(
-            List<Long> ids,
-            String authorization);
+  void ensureCanReadManagementResource(
+    Long idOffice,
+    String key,
+    String authorization
+  );
 
-    void ensureCanEditResource(Long idOffice);
+  void ensureCanEditResource(
+    List<Long> ids,
+    String authorization
+  );
 
 }
