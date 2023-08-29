2ff24d13823f429dfe445d39c815dd28ad5e5acb
icarosbmartins <icaro.martins@totemti.com.br>
2023-08-29 17:15:39 -0400

    chore: ajusta homologação

--- src/main/java/br/gov/es/openpmo/service/dashboards/v2/PurgeDashboards.java
+++ src/main/java/br/gov/es/openpmo/service/dashboards/v2/PurgeDashboards.java
@@ -3,15 +3,18 @@ package br.gov.es.openpmo.service.dashboards.v2;
 import br.gov.es.openpmo.model.dashboards.Dashboard;
 import br.gov.es.openpmo.model.workpacks.Workpack;
 import br.gov.es.openpmo.repository.dashboards.DashboardRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 @Component
 public class PurgeDashboards {
 
+  private final Logger logger = LoggerFactory.getLogger(PurgeDashboards.class);
   private final DashboardRepository dashboardRepository;
   private final IAsyncDashboardService asyncDashboardService;
 
@@ -26,16 +29,20 @@ public class PurgeDashboards {
   @Transactional
   public void execute() {
     final Iterable<Dashboard> dashboards = this.dashboardRepository.findAll();
-    final Collection<Workpack> workpacks = new ArrayList<>();
+    final List<Workpack> workpacks = new ArrayList<>();
     for (Dashboard dashboard : dashboards) {
       if (dashboard.getWorkpack() != null) {
         workpacks.add(dashboard.getWorkpack());
       }
     }
     this.dashboardRepository.deleteAll(dashboards);
+    logger.info("Início do cálculo dos dashboards!");
+    final int size = workpacks.size();
     for (Workpack workpack : workpacks) {
+      logger.info("Calculando dashboard {} de {}.", workpacks.indexOf(workpack) + 1, size);
       this.asyncDashboardService.calculate(workpack.getId(), true);
     }
+    logger.info("Fim do cálculo dos dashboards!");
   }
 
 }
