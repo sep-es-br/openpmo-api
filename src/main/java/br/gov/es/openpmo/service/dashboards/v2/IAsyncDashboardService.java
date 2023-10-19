c26be1d3740b1d4201a966ae14ccb894ea11e1df
icarosbmartins <icaro.martins@totemti.com.br>
2023-10-19 10:17:50 -0400

    chore: ajusta homologação

--- src/main/java/br/gov/es/openpmo/service/dashboards/v2/IAsyncDashboardService.java
+++ src/main/java/br/gov/es/openpmo/service/dashboards/v2/IAsyncDashboardService.java
@@ -1,10 +1,13 @@
 package br.gov.es.openpmo.service.dashboards.v2;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.transaction.annotation.Transactional;
+
 @FunctionalInterface
 public interface IAsyncDashboardService {
 
-//  @Async
-//  @Transactional
+  @Async
+  @Transactional
   void calculate(Long workpackId, Boolean calculateInterval);
 
 }
