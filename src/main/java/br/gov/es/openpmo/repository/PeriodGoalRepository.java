3854cba00e1ba9b15b88829b08827d756e7df493
João Pedro Faria <joao.almeida@totemti.com.br>
2025-02-20 16:26:25 -0300

    Indicadores de Projeto
    - Correçõe na hora de alterar valores de um indicador. Anteriormente, ele só adicionava mais períodos e sobrecarregava o banco

--- src/main/java/br/gov/es/openpmo/repository/PeriodGoalRepository.java
+++ src/main/java/br/gov/es/openpmo/repository/PeriodGoalRepository.java
@@ -1,7 +1,13 @@
 package br.gov.es.openpmo.repository;
 
 import br.gov.es.openpmo.model.indicators.PeriodGoal;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface PeriodGoalRepository extends Neo4jRepository<PeriodGoal, Long> {
+
+    @Query("MATCH (i:Indicator)-->(p:PeriodGoal) " +
+            "WHERE id(i) = $idIndicator " +
+            "DETACH DELETE p")
+    void deleteAllPeriodFromIndicatorId(Long idIndicator);
 }
